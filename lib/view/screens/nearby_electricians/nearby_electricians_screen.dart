b4edbeb702a43a60fe514a4262b191e83b32d98a
Fatma <btaelkholy408@gmail.com>
2026-05-04 16:51:42 +0300

    push

--- lib/view/screens/nearby_electricians/nearby_electricians_screen.dart
+++ lib/view/screens/nearby_electricians/nearby_electricians_screen.dart
@@ -75,137 +75,136 @@ class _NearbyElectriciansScreenState extends State<NearbyElectriciansScreen> {
     return Scaffold(
       backgroundColor: ColorResources.getScaffoldBackgroundColor(context),
       appBar: CustomAppBar(title: getTranslated('nearby_electricians', context)),
-      body: SafeArea(
-        child: Consumer<CustomAuthProvider>(
-          builder: (context, auth, child) {
-            return Column(
-              children: [
-                Expanded(
-                  child: Consumer2<CustomAuthProvider, SplashProvider>(
-                    builder: (context, auth, splashProvider, child) {
-                      return Scrollbar(
-                        child: SingleChildScrollView(
-                          padding: EdgeInsets.all(25),
-                          physics: const BouncingScrollPhysics(),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              ListView.builder(
-                                itemCount: widget.electricians.length,
-                                shrinkWrap: true,
-                                physics: const NeverScrollableScrollPhysics(),
-                                itemBuilder: (context, index) {
-                                  ElectricianModel electrician = widget.electricians[index];
-                                  AreaModel area = electrician.area!;
-                                  return Container(
-                                    padding: EdgeInsets.all(15),
-                                    margin: const EdgeInsets.only(bottom: 15),
-                                    decoration: BoxDecoration(
-                                      color: ColorResources.getTextFieldFillColor(context),
-                                      borderRadius: BorderRadius.circular(15)),
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        Row(
-                                          children: [
-                                            ClipRRect(
-                                              borderRadius: BorderRadius.circular(15),
-                                              child: CachedNetworkImage(
-                                                height: 80,
-                                                width: 80,
-                                                fit: BoxFit.cover,
-                                                imageUrl: "${splashProvider.configModel?.baseUrls?.electricianImageUrl}/${electrician.image}",
-                                                cacheKey: "${splashProvider.configModel?.baseUrls?.electricianImageUrl}/${electrician.image}",
-                                              ),
+      body: Consumer<CustomAuthProvider>(
+        builder: (context, auth, child) {
+          return Column(
+            children: [
+              Expanded(
+                child: Consumer2<CustomAuthProvider, SplashProvider>(
+                  builder: (context, auth, splashProvider, child) {
+                    return Scrollbar(
+                      child: SingleChildScrollView(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        physics: const BouncingScrollPhysics(),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ListView.builder(
+                              itemCount: widget.electricians.length,
+                              shrinkWrap: true,
+                              padding: EdgeInsets.zero,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemBuilder: (context, index) {
+                                ElectricianModel electrician = widget.electricians[index];
+                                AreaModel area = electrician.area!;
+                                return Container(
+                                  padding: EdgeInsets.all(15),
+                                  margin: const EdgeInsets.only(bottom: 15),
+                                  decoration: BoxDecoration(
+                                    color: ColorResources.getTextFieldFillColor(context),
+                                    borderRadius: BorderRadius.circular(15)),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          ClipRRect(
+                                            borderRadius: BorderRadius.circular(15),
+                                            child: CachedNetworkImage(
+                                              height: 80,
+                                              width: 80,
+                                              fit: BoxFit.cover,
+                                              imageUrl: "${splashProvider.configModel?.baseUrls?.electricianImageUrl}/${electrician.image}",
+                                              cacheKey: "${splashProvider.configModel?.baseUrls?.electricianImageUrl}/${electrician.image}",
                                             ),
-                                            SizedBox(width: 15),
-                                            Expanded(
-                                              child: Column(
-                                                crossAxisAlignment: CrossAxisAlignment.start,
-                                                children: [
-                                                  Text(
-                                                    electrician.name!,maxLines: 1,  overflow: TextOverflow.ellipsis ,
-                                                    style: TextStyle(
-                                                      color: ColorResources.getTextColor(context),
-                                                      fontSize: 16,
-                                                      fontWeight: FontWeight.w600,
-                                                    ),
+                                          ),
+                                          SizedBox(width: 15),
+                                          Expanded(
+                                            child: Column(
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  electrician.name!,maxLines: 1,  overflow: TextOverflow.ellipsis ,
+                                                  style: TextStyle(
+                                                    color: ColorResources.getTextColor(context),
+                                                    fontSize: 16,
+                                                    fontWeight: FontWeight.w600,
                                                   ),
-                                                  SizedBox(height: 2),
-                                                  Text(
-                                                    area.name!,maxLines: 1,  overflow: TextOverflow.ellipsis ,
-                                                    style: TextStyle(
-                                                      color: ColorResources.getTextColor(context).withOpacity(0.6),
-                                                      fontSize: 14,
-                                                      fontWeight: FontWeight.w400,
-                                                    ),
+                                                ),
+                                                SizedBox(height: 2),
+                                                Text(
+                                                  area.name!,maxLines: 1,  overflow: TextOverflow.ellipsis ,
+                                                  style: TextStyle(
+                                                    color: ColorResources.getTextColor(context).withOpacity(0.6),
+                                                    fontSize: 14,
+                                                    fontWeight: FontWeight.w400,
                                                   ),
-                                                  SizedBox(height: 2),
-                                                  Row(
-                                                    children: [
-                                                      Expanded(child: ElevatedButton.icon(
-                                                        icon: Icon(
-                                                          Icons.phone,
-                                                          color: ColorResources.getScaffoldBackgroundColor(context), size: 18,
+                                                ),
+                                                SizedBox(height: 2),
+                                                Row(
+                                                  children: [
+                                                    Expanded(child: ElevatedButton.icon(
+                                                      icon: Icon(
+                                                        Icons.phone,
+                                                        color: ColorResources.getScaffoldBackgroundColor(context), size: 18,
+                                                      ),
+                                                      label: Text(
+                                                        getTranslated('call_now', context),maxLines: 1,overflow: TextOverflow.ellipsis,
+                                                        style: TextStyle(
+                                                          color: ColorResources.getScaffoldBackgroundColor(context),
+                                                          fontSize: 15,
+                                                          fontWeight: FontWeight.w500,
                                                         ),
-                                                        label: Text(
-                                                          getTranslated('call_now', context),maxLines: 1,overflow: TextOverflow.ellipsis,
-                                                          style: TextStyle(
-                                                            color: ColorResources.getScaffoldBackgroundColor(context),
-                                                            fontSize: 15,
-                                                            fontWeight: FontWeight.w500,
-                                                          ),
+                                                      ),
+                                                      style: ElevatedButton.styleFrom(
+                                                        backgroundColor: ColorResources.getPrimaryColor(context),
+                                                        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                                                        shape: RoundedRectangleBorder(
+                                                          borderRadius: BorderRadius.circular(10),
                                                         ),
-                                                        style: ElevatedButton.styleFrom(
-                                                          backgroundColor: ColorResources.getPrimaryColor(context),
-                                                          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                                                          shape: RoundedRectangleBorder(
-                                                            borderRadius: BorderRadius.circular(10),
-                                                          ),
-                                                        ),
-                                                        onPressed: () => _launchCaller(electrician.phone!),
-                                                      ),),
+                                                      ),
+                                                      onPressed: () => _launchCaller(electrician.phone!),
+                                                    ),),
 
-                                                      SizedBox(width: 5),
-                                                      Expanded(child:   ElevatedButton.icon(
-                                                        icon: Icon(Icons.navigation, color: ColorResources.getScaffoldBackgroundColor(context), size: 18,),
-                                                        label: Text(getTranslated('navigate', context) ,maxLines: 1,overflow: TextOverflow.ellipsis,
-                                                          style: TextStyle(
-                                                              color: ColorResources.getScaffoldBackgroundColor(context),
-                                                              fontSize: 15 , fontWeight: FontWeight.w500
-                                                          ),),
-                                                        style: ElevatedButton.styleFrom(
-                                                          backgroundColor: ColorResources.getTextColor(context).withOpacity(0.6),
-                                                          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                                                          shape: RoundedRectangleBorder(
-                                                            borderRadius: BorderRadius.circular(10),
-                                                          ),
+                                                    SizedBox(width: 5),
+                                                    Expanded(child:   ElevatedButton.icon(
+                                                      icon: Icon(Icons.navigation, color: ColorResources.getScaffoldBackgroundColor(context), size: 18,),
+                                                      label: Text(getTranslated('navigate', context) ,maxLines: 1,overflow: TextOverflow.ellipsis,
+                                                        style: TextStyle(
+                                                            color: ColorResources.getScaffoldBackgroundColor(context),
+                                                            fontSize: 15 , fontWeight: FontWeight.w500
+                                                        ),),
+                                                      style: ElevatedButton.styleFrom(
+                                                        backgroundColor: ColorResources.getTextColor(context).withOpacity(0.6),
+                                                        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                                                        shape: RoundedRectangleBorder(
+                                                          borderRadius: BorderRadius.circular(10),
                                                         ),
-                                                        onPressed: () => _launchMaps(double.parse(electrician.latitude!), double.parse(electrician.longitude!)),
-                                                      ),)
-                                                    ],
-                                                  ),
-                                                ],
-                                              ),
+                                                      ),
+                                                      onPressed: () => _launchMaps(double.parse(electrician.latitude!), double.parse(electrician.longitude!)),
+                                                    ),)
+                                                  ],
+                                                ),
+                                              ],
                                             ),
-                                          ],
-                                        ),
-                                      ],
-                                    ),
-                                  );
-                                },
-                              ),
-                            ],
-                          ),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              },
+                            ),
+                          ],
                         ),
-                      );
-                    },
-                  ),
+                      ),
+                    );
+                  },
                 ),
-              ],
-            );
-          },
-        ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
